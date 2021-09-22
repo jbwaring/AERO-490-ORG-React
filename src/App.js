c8de4049f632e1715851ce0d2bd9dd8d9d1e8468
jbwaring <56231422+jbwaring@users.noreply.github.com>
2021-09-22 15:24:50 -0400

    updated org
    updated new PM roles

--- src/App.js
+++ src/App.js
@@ -24,8 +24,8 @@ function App() {
                   </div>
       <div className="revision-notes">
       <h3>Version Notes:</h3>
-      <p>Version 1.3.1 <br />
-      Updated organization.<br />
+      <p>Version 1.3.2 <br />
+      Updated organization and new PM Roles.<br />
       Removed one student who switched Capstone.
         </p>
       <a href="mailto:j_warin@live.concordia.ca?subject=Org%20Chart%20Website%20Feedback&body=Version%20Used%3A%0D%0AWhat%20should%20I%20improve%3F%3A" className="link-blue-std" id="feedback-link-bottom">
